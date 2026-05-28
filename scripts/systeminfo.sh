6840b863804488c06305c9cbd1bcf0d71e4cd000
Mah Variyani <108659794+mah404@users.noreply.github.com>
2026-05-28 08:25:46 +0200

    Add systeminfo script

---
+++ scripts/systeminfo.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "========================"
+echo " Systeminformationen"
+echo "========================"
+echo "Benutzer: $USER"
+echo "Hostname: $(hostname)"
+echo "Datum und Uhrzeit: $(date)"
+echo "Aktuelles Verzeichnis: $(pwd)"
